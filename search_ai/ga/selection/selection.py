641c37e5153ab86781eb3dbaec1b37800ecb77e6
Fabio Vital <fabiovital5@gmail.com>
2019-07-03 18:29:07 +0100

    Test ga.selection
     - Add 'number_of_parents' property to ga.selection.Selection
     - Test ga.selection.Selection class

--- search_ai/ga/selection/selection.py
+++ search_ai/ga/selection/selection.py
@@ -3,8 +3,21 @@ from abc import ABC, abstractmethod
 
 class Selection(ABC):
 
-    def __init__(self, num_of_parents):
-        self._number_of_parents = num_of_parents
+    MIN_NUMBER_OF_PARENTS = 2
+
+    def __init__(self, number_of_parents):
+        self._number_of_parents = number_of_parents
+
+    @property
+    def number_of_parents(self):
+        return self._number_of_parents
+
+    @number_of_parents.setter
+    def number_of_parents(self, number_of_parents):
+        if number_of_parents < MIN_NUMBER_OF_PARENTS:
+            raise ValueError("'number_of_parents' should be greater than " + MIN_NUMBER_OF_PARENTS)
+
+        self._number_of_parents = number_of_parents
 
     @abstractmethod
     def run(self, population):
