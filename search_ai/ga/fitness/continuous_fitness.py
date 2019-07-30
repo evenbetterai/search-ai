f684ca722cd4ae834272169f1555035835eaf53c
Fabio Vital <fabiovital5@gmail.com>
2019-07-30 21:27:21 +0100

    continuous fitness class

---
+++ search_ai/ga/fitness/continuous_fitness.py
@@ -0,0 +1,26 @@
+import random as rd
+
+from search_ai.ga.fitness.fitness import Fitness
+from search_ai.ga.individual.continuous_individual import ContinuousIndividual
+
+
+class ContinuousFitness(Fitness):
+
+    def __init__(self, len_features, features_info):
+        super(ContinuousFitness, self).__init__(len_features)
+        self._features_info = features_info
+
+    @property
+    def features_info(self):
+        return self._features_info
+
+    def new_blank_individual(self):
+        return ContinuousIndividual(self._len_features, self._features_info)
+
+    def new_random_individual(self):
+        ind = self.new_blank_individual()
+
+        for i in range(len(ind.features)):
+            ind.features[i] = rd.uniform(ind.features_info[i].min_value, ind.features_info[i].max_value)
+
+        return ind
