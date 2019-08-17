fa11bc4f346b29eca85387d7eadd21c9618b2f0f
Fabio Vital <fabiovital@live.com.pt>
2019-08-17 09:22:08 +0100

    mock individuals' features with a property mock

--- search_ai/tests/ga/replace_duplicates/test_replace_duplicates_with_randoms.py
+++ search_ai/tests/ga/replace_duplicates/test_replace_duplicates_with_randoms.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+import unittest.mock as mock
 
 from search_ai.ga.replace_duplicates.replace_duplicates_with_randoms import ReplaceDuplicatesWithRandoms
 from search_ai.tests.ga.test_case_with_population import TestCaseWithPopulation
@@ -8,12 +8,15 @@ class TestReplaceDuplicatesWithRandoms(TestCaseWithPopulation):
 
     def setUp(self):
         self.u = 10
-        self.population = self.create_population(self.u)
-        self.children = list(self.population[0:self.u]) + self.create_population(self.u, None, [list(map(lambda i: i+1, self.population[0].features)) for _ in range(self.u)])
+        self.diff = 2
+        self.population = [self._mock_individual([1]) for _ in range(self.u - self.diff)]
+        self.population += [self._mock_individual([2]) for _ in range(self.diff)]
+        self.children = [self._mock_individual([3]) for _ in range(self.u * 2 - self.diff * 3)]
+        self.children += [self._mock_individual([2]) for _ in range(self.diff * 3)]
 
-        self.fitness = MagicMock()
-        self.random_ind = self.create_population(1)[0]
-        self.fitness.new_random_individual.return_value = self.random_ind
+        self.new_children = [mock.MagicMock() for _ in range(self.diff * 3)]
+        self.fitness = mock.MagicMock()
+        self.fitness.new_random_individual.side_effect = self.new_children
 
         self.replace_duplicates = ReplaceDuplicatesWithRandoms(self.fitness)
 
@@ -21,3 +24,10 @@ class TestReplaceDuplicatesWithRandoms(TestCaseWithPopulation):
         self.replace_duplicates.run(self.population, self.children)
         self.assertEqual(self.fitness.new_random_individual.call_count, self.u)
         self.cmp_arrays(self.children[0:self.u], [self.random_ind] * self.u)
+
+    def _mock_individual(self, return_value):
+        ind = {}
+        ind['feature_prop_mock'] = mock.PropertyMock(return_value=return_value)
+        ind['obj'] = mock.MagicMock()
+        ind['obj'].features = ind['feature_prop_mock']
+        return ind
