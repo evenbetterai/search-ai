6485602e09f10c7c19967077e27f454a4ebf2603
Fabio Vital <fabiovital@live.com.pt>
2019-09-11 22:12:22 +0100

    FitnessHolder pydoc

--- search_ai/ga/fitness_holder.py
+++ search_ai/ga/fitness_holder.py
@@ -2,12 +2,33 @@ from abc import ABC
 
 
 class FitnessHolder(ABC):
+    """Simple base class to hold a fitness.fitness.Fitness object.
+
+    Note
+    ----
+    No validations are performed to check if the passed object extends
+    from fitness.fitness.Fitness.
+
+    See Also
+    --------
+    fitness : Fitness sub-module.
+
+    """
     
     def __init__(self, fitness):
+        """Initialize FitnessHolder, by storing `fitness`.
+
+        Parameters
+        ----------
+        fitness : fitness.fitness.Fitness
+            Object to hold.
+
+        """
         self._fitness = fitness
     
     @property
     def fitness(self):
+        """fitness.fitness.Fitness: Get or set object being hold."""
         return self._fitness
     
     @fitness.setter
